48c60d0d0688860aa99050a9778dc80489840f00
OzKitsune <54425262+OzKitsune@users.noreply.github.com>
2026-03-04 11:16:07 +1000

    Add TimerLabel

---
+++ source/UI/TimerLabel.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class TimerLabel : Label
+{
+	[Export] private Timer _timer;
+
+	public override void _Process(double delta)
+	{
+		Text = _timer.TimeLeft.ToString("F0");
+	}
+}
