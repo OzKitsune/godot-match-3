5dce58525289aa4a22bf245b95689a4f33197d82
OzKitsune <54425262+OzKitsune@users.noreply.github.com>
2026-03-06 10:01:44 +1000

    Update TimerLabel.cs

--- source/UI/TimerLabel.cs
+++ source/UI/TimerLabel.cs
@@ -5,7 +5,7 @@ public partial class TimerLabel : Label
 {
 	[Export] private Timer _timer;
 
-	public override void _Process(double delta)
+    public override void _Process(double delta)
 	{
 		Text = _timer.TimeLeft.ToString("F0");
 	}
