0ed0be853f1ddfb36d2a7b35d472c3806159a34c
OzKitsune <54425262+OzKitsune@users.noreply.github.com>
2026-03-06 10:03:09 +1000

    Add Game.cs

---
+++ source/Game.cs
@@ -0,0 +1,59 @@
+using Godot;
+using System;
+
+public partial class Game : Node2D
+{
+    [Export] private Board _board;
+
+    [Export] private Timer _timer;
+	[Export] private double _gameTime = 60.0;
+
+    [Export] private ScoreLabel _scoreLabel;
+    [Export] private Control _gameOverScreen;
+
+	public GameState State { get; set; } = GameState.Input;
+
+    private int _score = 0;
+
+    public override void _Ready()
+    {
+        _board.AnimationStarted += OnBoardAnimationStarted;
+        _board.AnimationFinished += OnBoardAnimationFinished;
+
+        _timer.Timeout += End;
+
+        Start();
+    }
+
+    private void Start()
+    {
+        _timer.Start(_gameTime);
+
+        _board.Fill();
+    }
+
+    public void AddScore(int score) 
+    {
+        _score += score;
+        _scoreLabel.SetScore(_score);
+    }
+
+    private void OnBoardAnimationStarted()
+    {
+        State = GameState.Busy;
+    }
+
+    private void OnBoardAnimationFinished()
+    {
+        State = GameState.Input;
+    }
+
+	private void End()
+    {
+        State = GameState.GameOver;
+
+        _board.ProcessMode = ProcessModeEnum.Disabled;
+
+		_gameOverScreen.Show();
+    }
+}
