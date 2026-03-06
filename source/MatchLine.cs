d81a41c6e5a2d8c0c7c689dd3fe66f3bf95238e0
OzKitsune <54425262+OzKitsune@users.noreply.github.com>
2026-03-06 11:57:43 +1000

    Update MatchLine.cs

--- source/MatchLine.cs
+++ source/MatchLine.cs
@@ -4,13 +4,15 @@ using System;
 public partial class MatchLine : RefCounted
 {
     public Type Type { get; }
+    public Color Color { get; }
     public Direction Direction { get; }
     public Vector2I[] Points { get; }
     public int Length { get; }
 
-    public MatchLine(Type type, Direction direction, params Vector2I[] points)
+    public MatchLine(Type type, Color color, Direction direction, params Vector2I[] points)
     {
         Type = type;
+        Color = color;
         Direction = direction;
         Points = points;
         Length = points.Length;
