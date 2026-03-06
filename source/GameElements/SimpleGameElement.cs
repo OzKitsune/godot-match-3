ad4bdf2ad0fb824b02304a0e91e9c1386302359e
OzKitsune <54425262+OzKitsune@users.noreply.github.com>
2026-03-06 10:02:35 +1000

    Update SimpleGameElement.cs

--- source/GameElements/SimpleGameElement.cs
+++ source/GameElements/SimpleGameElement.cs
@@ -4,4 +4,21 @@ using System;
 [Tool]
 public partial class SimpleGameElement : AGameElement
 {
+    private static readonly PackedScene[] _scenes =
+    [
+        GD.Load<PackedScene>("res://scenes/GameElements/Circle.tscn"),
+        GD.Load<PackedScene>("res://scenes/GameElements/Diamond.tscn"),
+        GD.Load<PackedScene>("res://scenes/GameElements/Hexagon.tscn"),
+        GD.Load<PackedScene>("res://scenes/GameElements/Rhomb.tscn"),
+        GD.Load<PackedScene>("res://scenes/GameElements/Triangle.tscn"),
+    ]; 
+
+    public static SimpleGameElement CreateRandom()
+    {
+        var scene = _scenes[GD.RandRange(0, _scenes.Length - 1)];
+
+        var element = scene.Instantiate<SimpleGameElement>();
+        element.Type = element.GetType();
+        return element;
+    }
 }
